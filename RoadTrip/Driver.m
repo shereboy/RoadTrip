a269b9e3808981eb663783dc3a0864d7a3fc7923
Tolga Seremet <nospamfor-tolga@Tolga-MacBook-Pro.local>
2013-07-05 16:56:23 +0300

    edit

---
+++ RoadTrip/Driver.m
@@ -0,0 +1,17 @@
+//
+//  Driver.m
+//  RoadTrip
+//
+//  Created by Tolga Seremet on 7/5/13.
+//  Copyright (c) 2013 Tolga Seremet. All rights reserved.
+//
+
+#import "Driver.h"
+
+
+@implementation Driver
+
+@dynamic name;
+@dynamic truck;
+
+@end
---
+++ RoadTrip/Trucks.m
@@ -0,0 +1,19 @@
+//
+//  Trucks.m
+//  RoadTrip
+//
+//  Created by Tolga Seremet on 7/5/13.
+//  Copyright (c) 2013 Tolga Seremet. All rights reserved.
+//
+
+#import "Trucks.h"
+
+
+@implementation Trucks
+
+@dynamic model;
+@dynamic make;
+@dynamic topSpeed;
+
+
+@end
